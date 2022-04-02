6d84b48229027686aa9287096271a6a3d927bbe1
jodowd7 <55516704+jodowd7@users.noreply.github.com>
2022-04-02 17:24:12 -0500

    change

--- README.md
+++ README.md
@@ -17,5 +17,3 @@ predominantly used in various workplaces; job opportunities are abundant for tho
 
 
 [Link to Seattle Airbnb Crime Advisory: Datasets & Jupyter Notebooks](https://cocalc.com/share/public_paths/9f5e4e91cd5e0842e82c32209ca40da9b7e1b24f) 
-
-[Link to Google](https://www.google.com)
--- cocalc/link to source code to reproduce project .py
+++
@@ -1,5 +0,0 @@
-## Link to Seattle Airbnb Crime Advisory: Datasets & Jupyter Notebooks
-[Link to Seattle Airbnb Crime Advisory: Datasets & Jupyter Notebooks](https://cocalc.com/share/public_paths/9f5e4e91cd5e0842e82c32209ca40da9b7e1b24f)
-Link to Seattle Airbnb Crime Advisory: Datasets & Jupyter Notebooks
-[test](https://cocalc.com/share/public_paths/9f5e4e91cd5e0842e82c32209ca40da9b7e1b24f)
-[Link to Google](https://www.google.com)
